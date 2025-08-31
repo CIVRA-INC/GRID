a029c812c9efa1dd9a0c1236802af64cdec8fbfd
Assad Isah <nottherealalanturing@gmail.com>
2025-08-31 23:42:23 +0100

    refactor: Remove unnecessary comments and improve code clarity in Soulbound NFT contracts

--- packages/contracts/contracts/SoulboundNeighborhoodBadge.sol
+++ packages/contracts/contracts/SoulboundNeighborhoodBadge.sol
@@ -5,10 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/ISoulboundNeighborhoodBadge.sol";
 
-/**
- * @title SoulboundNeighborhoodBadge
- * @dev Implements a Soulbound NFT for resident badges. Updated for OpenZeppelin v5.
- */
+
 contract SoulboundNeighborhoodBadge is ERC721, Ownable, ISoulboundNeighborhoodBadge {
     uint256 private _nextTokenId;
     mapping(uint256 => uint256) public neighborhoodOf;
@@ -17,9 +14,6 @@ contract SoulboundNeighborhoodBadge is ERC721, Ownable, ISoulboundNeighborhoodBa
 
     constructor(address initialOwner) ERC721("GRID Neighborhood Badge", "GRID") Ownable(initialOwner) {}
 
-    /**
-     * @dev Mints a new badge. Can only be called by the contract owner (the Verifier contract).
-     */
     function safeMint(address to, uint256 _neighborhoodId) public override onlyOwner {
         uint256 tokenId = _nextTokenId++;
         neighborhoodOf[tokenId] = _neighborhoodId;
@@ -27,10 +21,7 @@ contract SoulboundNeighborhoodBadge is ERC721, Ownable, ISoulboundNeighborhoodBa
         emit BadgeMinted(to, tokenId, _neighborhoodId);
     }
 
-    /**
-     * @dev Overrides the internal _update hook to prevent all token transfers,
-     * making the token "soulbound." Only the initial mint (from address(0)) is allowed.
-     */
+   
     function _update(address from, address to, uint256[] memory tokenIds, uint256 batchSize) internal override {
         require(from == address(0), "SoulboundNeighborhoodBadge: Cannot be transferred");
         super._update(from, to, tokenIds, batchSize);
--- packages/contracts/contracts/Verifier.sol
+++ packages/contracts/contracts/Verifier.sol
@@ -4,12 +4,6 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/ISoulboundNeighborhoodBadge.sol";
 
-/**
- * @title Verifier
- * @dev This contract orchestrates the user verification process. It requests
- * attestations from the Flare State Connector and, upon success, directs the
- * SBNFT contract to mint a badge.
- */
 contract Verifier is Ownable {
     ISoulboundNeighborhoodBadge public sbnftContract;
 
@@ -23,30 +17,17 @@ contract Verifier is Ownable {
         sbnftContract = ISoulboundNeighborhoodBadge(_sbnftAddress);
     }
 
-    /**
-     * @dev Kicks off the verification process for a user.
-     * In a real implementation, this would trigger a call to the State Connector.
-     */
     function requestVerification(string calldata locationData) external {
         uint256 requestId = _nextRequestId++;
         verificationRequests[requestId] = msg.sender;
         emit VerificationRequested(requestId, msg.sender, locationData);
-
-        // PRODUCTION TODO: Integrate the actual call to the Flare State Connector here.
-        // This involves formatting the request according to the attestation protocol.
     }
 
-    /**
-     * @dev Simulates the callback/response from the State Connector.
-     * In the real world, this function would be callable by the State Connector and
-     * would decode the attestation proof. For the MVP, it is restricted to the
-     * owner to manually confirm a successful off-chain verification.
-     */
+  
     function manualConfirmVerification(uint256 requestId, uint256 neighborhoodId) public onlyOwner {
         address user = verificationRequests[requestId];
         require(user != address(0), "Verifier: Invalid request ID");
 
-        // The Verifier contract, as the owner of the SBNFT contract, calls mint.
         sbnftContract.safeMint(user, neighborhoodId);
         emit VerificationConfirmed(requestId, user);
 
